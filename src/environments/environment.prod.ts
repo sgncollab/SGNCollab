e576f1d7e10cbaeca4a312f3c48ff7702781ff8a
Supriya Satre <supriyasatre@orient.com>
2020-08-21 18:48:55 +0530

    environment.prod.ts updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA7kKZPExuqrlMwd3Z3yw6nrq6npVTep3s",
+    authDomain: "sgn-collab.firebaseapp.com",
+    databaseURL: "https://sgn-collab.firebaseio.com",
+    projectId: "sgn-collab",
+    storageBucket: "sgn-collab.appspot.com",
+    messagingSenderId: "1085094950618",
+    appId: "1:1085094950618:web:f0291b263550a2c46397e3",
+    measurementId: "G-CNBCS70JMX"
+   }
 };
